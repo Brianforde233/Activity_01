1d580ae110b477af1af73774a26d71b22fbb0ec5
Brianforde233 <67812860+Brianforde233@users.noreply.github.com>
2020-08-25 10:43:45 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+* Prints a description of the course.
+*
+* Activity 1
+*@author Brian Forde -Comp1210-001
+*version 8/25/20
+*/
+public class StudentInfo {
+/**.
+* Prints description of course 
+*@param args Command line arguments- not used.
+*/
+   public static void main(String[] args) 
+   {
+      System.out.println("Name: Brian Forde");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+      
+   }
+}
